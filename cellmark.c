23f94dd3c012ea65d46ff5cbb856a48e171ba7cb
jorgicor <jorge.giner@hotmail.com>
2017-01-29 13:25:47 +0100

    * Factor the calculation of the word and bit in cellmark.

--- cellmark.c
+++ cellmark.c
@@ -6,6 +6,7 @@
 
 enum { NCELLMARK = (NCELL / 32) + ((NCELL % 32) ? 1 : 0) };
 
+/* We use 1 bit for each cell. */
 static unsigned int s_cellmarks[NCELLMARK];
 
 #ifdef RANGE_CHECK
@@ -16,37 +17,41 @@ static unsigned int s_cellmarks[NCELLMARK];
 #define check_marki(i)
 #endif
 
+static void calc_w_i(int celli, int *w, int *i)
+{
+	int wt;
+
+	check_celli(celli);
+	wt = celli >> 5;
+	check_marki(wt);
+	celli &= 31;
+	*w = wt;
+	*i = celli;
+}
+
 void mark_cell(int celli)
 {
 	int w;
 
-	check_celli(celli);
-	w = celli >> 5;
-	check_marki(w);
-	celli &= 31;
+	calc_w_i(celli, &w, &celli);
 	s_cellmarks[w] |= (1 << celli);
 }
 
-int cell_mark(int celli)
+int cell_marked(int celli)
 {
 	int w;
 
-	check_celli(celli);
-	w = celli >> 5;
-	check_marki(w);
-	celli &= 31;
+	calc_w_i(celli, &w, &celli);
 	return s_cellmarks[w] & (1 << celli);
 }
 
+/* If a cell is unmarked, mark it and return 1; else 0. */
 int if_cell_mark(int celli)
 {
 	int w;
 	unsigned int mask;
 
-	check_celli(celli);
-	w = celli >> 5;
-	check_marki(w);
-	celli &= 31;
+	calc_w_i(celli, &w, &celli);
 	mask = 1 << celli;
 	if (!(s_cellmarks[w] & mask)) {
 		s_cellmarks[w] |= mask;
@@ -56,15 +61,13 @@ int if_cell_mark(int celli)
 	return 0;
 }
 
+/* If a cell is marked, unmark it and return 1; else 0. */
 int if_cell_unmark(int celli)
 {
 	int w;
 	unsigned int mask;
 
-	check_celli(celli);
-	w = celli >> 5;
-	check_marki(w);
-	celli &= 31;
+	calc_w_i(celli, &w, &celli);
 	mask = 1 << celli;
 	if (s_cellmarks[w] & mask) {
 		s_cellmarks[w] &= ~mask;
--- cellmark.h
+++ cellmark.h
@@ -1,8 +1,8 @@
 #ifndef CELLMARK_H
 #define CELLMARK_H
 
+int cell_marked(int celli);
 void mark_cell(int celli);
-int cell_mark(int celli);
 int if_cell_mark(int celli);
 int if_cell_unmark(int celli);
 void cellmark_init(void);
